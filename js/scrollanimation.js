d08f93f59a924d21fa6d36de10315296c3512d70
João Marcos <80593996+Joao-Sant-Ana@users.noreply.github.com>
2023-08-22 10:34:54 -0300

    correção

--- js/scrollanimation.js
+++ js/scrollanimation.js
@@ -1,9 +1,13 @@
+//Animação da scrollbar
+
 var progress = document.getElementById('progressbar')
 var totalHeight = document.body.scrollHeight - window.innerHeight
 
 window.addEventListener('scroll', scrollProgress)
 
 function scrollProgress () {
+
+    //Tamanho que a progressbar terá
     let progressHeight = (window.pageYOffset / totalHeight) *100
     progress.style.height = progressHeight + "%"
 }
